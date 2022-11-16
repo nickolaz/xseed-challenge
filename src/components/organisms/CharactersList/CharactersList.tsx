77ea0b65bc5023af67eccdba8a5bcdec23d59de8
Nicolas Ayala <nicolas.ayala@mangoapp.com.py>
2022-11-16 13:54:18 -0300

    add useMemo in CharactersList and optional loading

--- src/components/organisms/CharactersList/CharactersList.tsx
+++ src/components/organisms/CharactersList/CharactersList.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react';
+
 import { Grid } from '@mui/material';
 
 import { CharacterType } from '../../../utils';
@@ -5,7 +7,7 @@ import { Character, CharacterSkeleton } from '../../molecules';
 
 interface CharactersListProps {
   characteres?: CharacterType[];
-  loading: Boolean;
+  loading?: Boolean;
   updateFavorites: (character: CharacterType) => void;
 }
 const CharactersList = ({
@@ -16,26 +18,33 @@ const CharactersList = ({
   //Array of Fake Elements for fill the screen with skeletons
   const arrayOfTen = Array.from(Array(10).keys());
 
+  const skeletons = useMemo(
+    () =>
+      arrayOfTen.map((value) => (
+        <Grid item xs={4} sm={4} md={4} key={value}>
+          <CharacterSkeleton key={value} />
+        </Grid>
+      )),
+    [],
+  );
+
+  const characters = useMemo(
+    () =>
+      characteres?.map((character) => (
+        <Grid item xs={4} sm={4} md={4} key={character.name}>
+          <Character character={character} updateFavorites={updateFavorites} />
+        </Grid>
+      )),
+    [characteres, updateFavorites],
+  );
+
   return (
     <Grid
       container
       spacing={{ xs: 2, md: 3 }}
       columnSpacing={{ xs: 1, sm: 5, md: 8 }}
       columns={{ xs: 4, sm: 8, md: 8 }}>
-      {loading
-        ? arrayOfTen.map((value) => (
-            <Grid item xs={4} sm={4} md={4} key={value}>
-              <CharacterSkeleton key={value} />
-            </Grid>
-          ))
-        : characteres?.map((character) => (
-            <Grid item xs={4} sm={4} md={4} key={character.name}>
-              <Character
-                character={character}
-                updateFavorites={updateFavorites}
-              />
-            </Grid>
-          ))}
+      {loading ? skeletons : characters}
     </Grid>
   );
 };
